9a38468fb76eb76d1c67b9a293df4409e1c61143
cedric.ruehlicke <cedric.ruehlicke@ebsnet.de>
2020-02-20 11:05:44 +0100

    fix: better coloring of background tasks to fit into exit code colors

--- functions/__bobthefish_colors.fish
+++ functions/__bobthefish_colors.fish
@@ -454,7 +454,7 @@ function __bobthefish_colors -S -a color_scheme -d 'Define colors used by bobthe
 
       set -x color_initial_segment_exit  $red $fg  --bold
       set -x color_initial_segment_su    $fg $purple --bold
-      set -x color_initial_segment_jobs  $fg $comment --bold
+      set -x color_initial_segment_jobs  $red $comment --bold
 
       set -x color_path                  $selection $fg
       set -x color_path_basename         $selection $fg --bold
